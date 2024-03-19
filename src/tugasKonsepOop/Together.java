a01732cf6725666ba5220e813112339cb1ea2306
SaukiFutaki <utaki.sauki19@gmail.com>
2024-03-19 22:20:00 +0700

    finally

---
+++ src/tugasKonsepOop/Together.java
@@ -0,0 +1,12 @@
+package tugasKonsepOop;
+
+public class Together {
+  public static void main(String[] args) {
+    OpsiA A = new OpsiA();
+    OpsiB B = new OpsiB();
+   
+    A.printSegitigaAtas(5);
+    B.printSegitigaTerbalik(5);
+  }
+
+}
